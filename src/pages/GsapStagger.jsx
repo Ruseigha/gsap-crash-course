54ffa79a7e1ce1133d73b91d22ddff8ef333cc27
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-30 19:47:41 +0100

    feat: implement gsap.stagger() method for staggered animations

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      y: 220,
+      duration: 1,
+      ease: "power1.inOut",
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      borderRadius: "50%",
+      // stagger: 0.5,
+      stagger: {
+        amount: 1.5,
+        grid: [2, 1],
+        from: "center",
+      }
+    })
+  }, []);
 
   return (
     <main>
